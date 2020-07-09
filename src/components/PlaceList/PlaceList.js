e4df294ba2d721b3950a5080b7ccff488834cddf
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-07-09 11:54:25 +0200

    Add Card top PlaceList

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,6 +1,8 @@
 import React, {useContext,useCallback} from 'react';
 import {PlacesContext} from 'contexts/PlacesContext';
 
+import Card from 'components/UI/Card/Card';
+
 
 const  PlaceList = () => {
 
@@ -13,9 +15,7 @@ const  PlaceList = () => {
   return (
     <div>
       <input type="text" onChange={inputChange} />
-      <ul>
-        {list.filter(place=>place.visible).map(place => <li key={place.id}>{place.name}<br/><small>{place.address}</small></li> )}
-      </ul>
+        {list.filter(place=>place.visible).map(place => <Card key={place.id}>{place.name}<br/><small>{place.address}</small></Card> )}
     </div>
   )
 }
