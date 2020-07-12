f4c104155c283f5dc87c21949d0a3d846e41b74a
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-07-12 16:33:21 +0200

    Add user Icon.Avatar

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -2,23 +2,25 @@ import React, {useContext} from 'react';
 import {AuthContext} from 'contexts/AuthContext'
 import { Link} from "react-router-dom";
 import Styles from "./UserInfo.module.scss"
+import {FaUser} from 'react-icons/fa';
 
 function User() {
 
   const Auth = useContext(AuthContext);
 
-  const {name,id,email,loggedIn} = Auth.user
+  const {name,id,email,image,loggedIn} = Auth.user
   if(loggedIn) return (
     <div className={Styles.UserInfo}>
-      user: {name} <br/>
-      email: {email} <br/>
-      id: {id} <br/>
-      <button onClick={Auth.logout}>Logout</button>
+      <div className={Styles.UserIcon} onClick={Auth.logout}>
+        {image?(<div className={Styles.UserImage} style={{backgroundImage:`url(${image})`}}></div>):<FaUser/>}
+      </div>
     </div>
   )
   else return(
     <div className={Styles.UserInfo}>
-      <Link to="/login">Zaloguj</Link>
+      <Link to="/login" className={Styles.UserIcon}>
+        <FaUser/>
+      </Link>
     </div>
   )
 }
