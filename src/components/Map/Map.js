cf931be1deb28443aabaa8b188dbff6acab2dae6
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-07-12 09:49:34 +0200

    Save map instance, center, zoom to MapContext

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,52 +1,72 @@
-import React, {useContext} from 'react';
-import {mapComponent} from "./Map.module.scss"
+import React, {useState,useContext, useRef} from 'react';
+import styles from "./Map.module.scss"
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import {PlacesContext} from 'contexts/PlacesContext';
 import {MapContext} from 'contexts/MapContext';
 
 
 const libraries = ['geometry']
+let centered = false;
 
-const center = {
-  lat: 59.95,
-  lng: 30.33
+const Markers = () => {
+  const places = useContext(PlacesContext);
+  return places.list.filter(place=>place.visible && place.location.lat).map(place => <Marker position={place.location} key={place.id}/>)
 }
 
-let centered = false;
+const Places = (props) => {
+  const places = useContext(PlacesContext);
+  const map = useContext(MapContext);
+  const listeners = useRef(false)
+
+  if(!props.map) return false;
+
+  //Save map instance to context
+  if(!map.instance()) setTimeout(()=>{
+    map.instance(props.map);
+  },0);
+
+  //Save map center and zoom to context
+  if(!listeners.current) {
+    props.map.addListener('idle', ()=>{
+      map.center(props.map.getCenter().toJSON());
+      map.zoom(props.map.getZoom());
+    });
+    listeners.current = true;
+  }
+
+  // Fit bounds on first render;
+  if(!centered && places.list.length) {
+    const bounds = new window.google.maps.LatLngBounds();
+    places.list.forEach(place => bounds.extend(place.location))
+    props.map.fitBounds(bounds);
+    centered = true;
+  }
+  return false;
+}
 
 
 const Map = (props) => {
 
   const zoomPosition = (window.google && window.google.maps.ControlPosition.LEFT_BOTTOM) || null;
   const typeControl = (window.google && window.google.maps.MapTypeControlStyle.DROPDOWN_MENU) || null
-  const places = useContext(PlacesContext);
-  const map = useContext(MapContext);
+  const [map,setMap] = useState();
 
-  console.log('render');
-  // Fit bounds after first load
-  if(!centered && places.list.length && map.get) {
-    const bounds = new window.google.maps.LatLngBounds();
-    places.list.forEach(place => {
-      bounds.extend(place.location);
-    })
-    map.get.fitBounds(bounds);
-    centered = true;
-  }
   return(
-    <div className={mapComponent}>
+    <div className={styles.Map}>
+      <Places map={map}/>
       <LoadScript
         libraries={libraries}
         googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}
       >
       <GoogleMap
           mapContainerStyle={{height:'100%'}}
-          center={center}
+          center={{ lat:51, lng:13 }}
           zoom={10}
-          onLoad={mapEl => map.set(mapEl)}
+          onLoad={(map)=>setMap(map)}
           options={{streetViewControlOptions:{position:zoomPosition},zoomControlOptions:{position:zoomPosition},mapTypeControlOptions:{style:typeControl} }}
-          id="marker-example"
+          
         >
-          {places.list.filter(place=>place.visible && place.location.lat).map(place => <Marker position={place.location} key={place.id}/>)}
+          <Markers/>
         </GoogleMap>
       </LoadScript>
    
@@ -55,4 +75,4 @@ const Map = (props) => {
 
 }
 
-export default Map;
+export default React.memo(Map);
--- src/contexts/MapContext.js
+++ src/contexts/MapContext.js
@@ -4,7 +4,25 @@ export const MapContext = React.createContext();
 
 const MapProvider = ({children}) => {
   const [map,setMap] = useState(null);
-  return <MapContext.Provider value={{get:map,set:setMap}}>{children}</MapContext.Provider>
+  const [mapCenter,setMapCenter] = useState({ lat:51, lng:13 })
+  const [mapZoom,setMapZoom] = useState(10);
+
+  const instance = (instance) => {
+    if(instance) setMap(instance);
+    return instance || map;
+  }
+
+  const center = (center = undefined) => {
+    if(!center) return mapCenter;
+    if(JSON.stringify(center) !== JSON.stringify(mapCenter)) setMapCenter(center);
+  }
+
+  const zoom = (zoom = undefined) => {
+    if(zoom) setMapZoom(zoom);
+    return zoom || mapZoom;
+  }
+
+  return <MapContext.Provider value={{instance,center,zoom}}>{children}</MapContext.Provider>
 }
 
 export default MapProvider;
