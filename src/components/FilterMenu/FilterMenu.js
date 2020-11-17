e4a32ad170999c285b0e00a9fbfb367ecbc5c0fa
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-11-17 17:58:03 +0100

    Add type buttons to filter menu

--- src/components/FilterMenu/FilterMenu.js
+++ src/components/FilterMenu/FilterMenu.js
@@ -3,8 +3,22 @@ import { PlacesContext } from "contexts/PlacesContext";
 import Button from "components/UI/Button/Button";
 import Toggle from "components/UI/Toggle/Toggle";
 import PopMenu from "components/UI/PopMenu/PopMenu";
+import { types } from "config/placeTypes/";
+import Styles from "./FilterMenu.module.scss";
 
 import { FaSlidersH as Filter } from "react-icons/fa";
+
+const TypesOptions = (props) => {
+  const typeBtns = [];
+  for (const type in types) {
+    typeBtns.push(
+      <Button flat={true}>
+        <span style={{ fill: types[type].color }}>{types[type].icon}</span>
+      </Button>
+    );
+  }
+  return <div className={Styles.FilterMenu}>{typeBtns}</div>;
+};
 
 export default (props) => {
   const { mapFilter, toggleMapFilter } = useContext(PlacesContext);
@@ -15,11 +29,14 @@ export default (props) => {
   );
   return (
     <PopMenu component={button}>
-      <Toggle
-        state={mapFilter}
-        onChange={toggleMapFilter}
-        label="Filtruj po mapie"
-      />
+      <TypesOptions />
+      <div className={Styles.FilterOptions}>
+        <Toggle
+          state={mapFilter}
+          onChange={toggleMapFilter}
+          label="Filtruj po mapie"
+        />
+      </div>
     </PopMenu>
   );
 };
