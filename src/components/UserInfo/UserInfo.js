49be1acb348d3247276b96f03b3694ef7f602369
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-11-17 20:41:56 +0100

    Add User menu

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -2,28 +2,48 @@ import React, { useContext } from "react";
 import { AuthContext } from "contexts/AuthContext";
 import { Link } from "react-router-dom";
 import Styles from "./UserInfo.module.scss";
-import { RiUser3Line as UserIcon } from "react-icons/ri";
+import { RiUser4Line as UserIcon } from "react-icons/ri";
+import PopMenu from "components/UI/PopMenu/PopMenu";
+
+const LoggedIn = (props) => {
+  //const { name, id, email, image } = props.user;
+  const { image } = props.user;
+  console.log("turutu", image);
+  return (
+    <div
+      className={Styles.UserInfo}
+      style={props.style}
+      onClick={props.onClick}
+    >
+      <div className={Styles.UserIcon}>
+        {image ? (
+          <div
+            className={Styles.UserImage}
+            style={{ backgroundImage: `url(${image})` }}
+          ></div>
+        ) : (
+          <UserIcon />
+        )}
+      </div>
+    </div>
+  );
+};
 
 const User = (props) => {
   const Auth = useContext(AuthContext);
 
-  const { name, id, email, image, loggedIn } = Auth.user;
-  if (loggedIn)
+  const { loggedIn } = Auth.user;
+  if (loggedIn) {
     return (
-      <div className={Styles.UserInfo} style={props.style}>
-        <div className={Styles.UserIcon} onClick={Auth.logout}>
-          {image ? (
-            <div
-              className={Styles.UserImage}
-              style={{ backgroundImage: `url(${image})` }}
-            ></div>
-          ) : (
-            <UserIcon />
-          )}
-        </div>
+      <div className="dupa" style={props.style}>
+        <PopMenu component={<LoggedIn user={Auth.user} />}>
+          <a href="#!" onClick={Auth.logout}>
+            Wyloguj
+          </a>
+        </PopMenu>
       </div>
     );
-  else
+  } else
     return (
       <div className={Styles.UserInfo} style={props.style}>
         <Link to="/login" className={Styles.UserIcon}>
