d70ee501c28cd77bfafa5ad95dd3706ad909ddb3
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-11-17 18:44:13 +0100

    add off & disabled states to Button compononent

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import Styles from './Button.module.scss';
+import React from "react";
+import Styles from "./Button.module.scss";
 
 const Button = (props = {}) => {
   return (
-    <button className={`${Styles.Button} ${props.flat ? Styles.flat : ''}`} onClick={props.onClick}>{props.children}</button>
-  )
-}
+    <button
+      className={`${Styles.Button} ${props.flat ? Styles.flat : ""} ${
+        props.off ? Styles["Button--off"] : ""
+      }`}
+      disabled={props.disabled || false}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
