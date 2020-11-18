39b3e8a35c1dd7bcd4eb86b351a05dc141853da0
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-11-18 19:51:38 +0100

    Change Popmenu position to fixed

--- src/components/UI/PopMenu/PopMenu.js
+++ src/components/UI/PopMenu/PopMenu.js
@@ -17,10 +17,40 @@ export default (props) => {
     }
   };
 
+  const positionMenu = () => {
+    if (!containerRef.current) return false;
+    const menu = containerRef.current.querySelector(`.${Styles.PopMenu}`);
+    if (!menu) return false;
+    const menuContainerBoundingRect = containerRef.current.getBoundingClientRect();
+    const menuBoundingRect = menu.getBoundingClientRect();
+    menu.style.top = `${
+      menuContainerBoundingRect.top + menuContainerBoundingRect.height + 10
+    }px`;
+    menu.style.left = `${
+      menuContainerBoundingRect.left + menuContainerBoundingRect.width / 2
+    }px`;
+  };
+
   useEffect(() => {
     document.addEventListener("click", hideMenu);
     return () => document.removeEventListener("click", hideMenu);
-  });
+  }, []);
+
+  useEffect(() => {
+    console.log("add menu listener");
+    window.addEventListener("scroll", positionMenu);
+    window.addEventListener("resize", positionMenu);
+    return () => {
+      window.removeEventListener("scroll", positionMenu);
+      window.removeEventListener("resize", positionMenu);
+    };
+  }, []);
+
+  if (menu)
+    requestAnimationFrame(() => {
+      positionMenu();
+    });
+
   if (!props.component) return false;
   return (
     <div ref={containerRef} className={Styles.PopMenu__container}>
