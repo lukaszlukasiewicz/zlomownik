92cf91316f83631001edd250b4e008bb494fa17f
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-11-19 09:42:30 +0100

    Add basic styling for PlaceInfo component

--- src/components/PlaceInfo/PlaceInfo.js
+++ src/components/PlaceInfo/PlaceInfo.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { PlacesContext } from "contexts/PlacesContext";
+import Styles from "./PlaceInfo.module.scss";
 
 export default (props) => {
   const { list } = useContext(PlacesContext);
@@ -9,13 +10,23 @@ export default (props) => {
   const history = useHistory();
   console.log(place);
   if (!place) return false;
+  console.log(place.location);
   return (
-    <div>
+    <div className={Styles.PlaceInfo}>
       <div>
         <button onClick={(e) => history.goBack()}>Wróć</button>
       </div>
-      <img src={place.image} alt={place.name} />
-      <h1>{place.name}</h1>
+      <div className={Styles.PlaceInfo__gallery}>
+        <div
+          className={Styles.PlaceInfo__image}
+          style={{ backgroundImage: `url(${place.image})` }}
+        ></div>
+      </div>
+      <h1 className={Styles.PlaceInfo__name}>{place.name}</h1>
+      <p className={Styles.PlaceInfo__address}>{place.address}</p>
+      <p
+        className={Styles.PlaceInfo__location}
+      >{`GPS: ${place.location.lat} ${place.location.lng}`}</p>
     </div>
   );
 };
